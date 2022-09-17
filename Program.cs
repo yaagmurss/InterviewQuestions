5715b9959244bf1261d26448268f71f857c8b13b
Yagmur <52479605+yaagmurss@users.noreply.github.com>
2022-09-17 22:50:30 +0300

    Add files via upload
    RunningArrays

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using InterwiewQuestions;
+
+
+namespace InterwiewQuestions
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[] { 1, 2, 3, 4, 5 };
+
+            var runningSumfOfArray = new RunningSumfOf1dArray();
+            runningSumfOfArray.RunningSum(nums);
+        }
+    }
+}
