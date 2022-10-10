4774cbe05140928b3c7bcfe52751b8d0456ac4cd
Yagmur <52479605+yaagmurss@users.noreply.github.com>
2022-10-10 09:15:53 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+
+namespace DataStructureAndAlgorithms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StacksClass sampleStack = new StacksClass();
+            sampleStack.SampleStacks();
+        }
+    }
+}
