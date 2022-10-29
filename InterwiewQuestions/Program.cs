a16d7b411430e2a848c754d8d37ee3f50ddab2c0
Yagmur <52479605+yaagmurss@users.noreply.github.com>
2022-10-29 11:48:44 +0300

    Add files via upload
    Binary Search questions added

--- InterwiewQuestions/Program.cs
+++ InterwiewQuestions/Program.cs
@@ -102,6 +102,48 @@ namespace InterwiewQuestions
             if (easyQuestions.RemoveElement(testCase5ForRemoveElement,1) == 0) { Console.WriteLine("testCase5 : PASS :)"); } else { Console.WriteLine("testCase5 : FAIL"); }
             if (easyQuestions.RemoveElement(testCase6ForRemoveElement,1) == 1) { Console.WriteLine("testCase6 : PASS :)"); } else { Console.WriteLine("testCase6 : FAIL"); }
 
+
+            Console.WriteLine("---------------------SearchInsertPosition----------------------");
+
+            var binaySearchQuestions = new BinaySearchQuestions();
+
+            var testCase1ForSearchInsertPosition = new int[] { 1, 3, 5, 6 };
+            var testCase2ForSearchInsertPosition = new int[] { 1, 3, 5, 6 };
+            var testCase3ForSearchInsertPosition = new int[] { 1, 3, 5, 6 };
+            var testCase4ForSearchInsertPosition = new int[] { };
+            var testCase5ForSearchInsertPosition = new int[] { 1 };
+            var testCase6ForSearchInsertPosition = new int[] { 1, 2, 3 };
+
+            //Console.WriteLine(easyQuestions.RemoveDuplicates(numbers));
+            if (binaySearchQuestions.SearchInsertPosition(testCase1ForSearchInsertPosition, 5) == 2) { Console.WriteLine("testCase1 : PASS :)"); } else { Console.WriteLine("testCase1 : FAIL"); }
+            if (binaySearchQuestions.SearchInsertPosition(testCase2ForSearchInsertPosition, 2) == 1) { Console.WriteLine("testCase2 : PASS :)"); } else { Console.WriteLine("testCase2 : FAIL"); }
+            if (binaySearchQuestions.SearchInsertPosition(testCase3ForSearchInsertPosition, 7) == 4) { Console.WriteLine("testCase3 : PASS :)"); } else { Console.WriteLine("testCase3 : FAIL"); }
+            if (binaySearchQuestions.SearchInsertPosition(testCase4ForSearchInsertPosition, 5) == 0) { Console.WriteLine("testCase4 : PASS :)"); } else { Console.WriteLine("testCase4 : FAIL"); }
+            if (binaySearchQuestions.SearchInsertPosition(testCase5ForSearchInsertPosition, 2) == 1) { Console.WriteLine("testCase5 : PASS :)"); } else { Console.WriteLine("testCase5 : FAIL"); }
+            if (binaySearchQuestions.SearchInsertPosition(testCase6ForSearchInsertPosition, 0) == 0) { Console.WriteLine("testCase6 : PASS :)"); } else { Console.WriteLine("testCase6 : FAIL"); }
+
+            Console.WriteLine("---------------------LengthOfLastWord----------------------");
+
+            var stringOperationsQuestions = new StringOperationsQuestions();
+
+            var testCase1ForStringOperationsQuestions = "Hello World";
+            var testCase2ForStringOperationsQuestions = "   fly me   to   the moon  ";
+            var testCase3ForStringOperationsQuestions = "luffy is still joyboy";
+            var testCase4ForStringOperationsQuestions = "     ";
+            var testCase5ForStringOperationsQuestions = "a";
+            var testCase6ForStringOperationsQuestions = "aaa";
+
+            //Console.WriteLine(easyQuestions.RemoveDuplicates(numbers));
+            if (stringOperationsQuestions.LengthOfLastWord(testCase1ForStringOperationsQuestions) == 5) { Console.WriteLine("testCase1 : PASS :)"); } else { Console.WriteLine("testCase1 : FAIL"); }
+            if (stringOperationsQuestions.LengthOfLastWord(testCase2ForStringOperationsQuestions) == 4) { Console.WriteLine("testCase2 : PASS :)"); } else { Console.WriteLine("testCase2 : FAIL"); }
+            if (stringOperationsQuestions.LengthOfLastWord(testCase3ForStringOperationsQuestions) == 6) { Console.WriteLine("testCase3 : PASS :)"); } else { Console.WriteLine("testCase3 : FAIL"); }
+            if (stringOperationsQuestions.LengthOfLastWord(testCase4ForStringOperationsQuestions) == 0) { Console.WriteLine("testCase4 : PASS :)"); } else { Console.WriteLine("testCase4 : FAIL"); }
+            if (stringOperationsQuestions.LengthOfLastWord(testCase5ForStringOperationsQuestions) == 1) { Console.WriteLine("testCase5 : PASS :)"); } else { Console.WriteLine("testCase5 : FAIL"); }
+            if (stringOperationsQuestions.LengthOfLastWord(testCase6ForStringOperationsQuestions) == 3) { Console.WriteLine("testCase6 : PASS :)"); } else { Console.WriteLine("testCase6 : FAIL"); }
+
+
+
+
         }
     }
 }
