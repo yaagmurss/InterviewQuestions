31949ebad07a288a1d42dbe86b61537773bb3634
Yagmur <52479605+yaagmurss@users.noreply.github.com>
2022-10-31 14:44:20 +0300

    Add files via upload

---
+++ InterwiewQuestions/InterwiewQuestions/StringOperationsQuestions.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Tracing;
+using System.Linq;
+using System.Text;
+
+namespace InterwiewQuestions
+{
+    internal class StringOperationsQuestions
+    {
+        public int LengthOfLastWord(string s)
+        {
+
+            bool isContainsAnyLetter = false; 
+
+            String[] sArray = s.Split(' ');
+
+            var sArray2 =new List<string>();
+
+            foreach (string str in sArray)
+            {
+                if (str != "")
+                {
+                    sArray2.Add(str);
+                    isContainsAnyLetter = true;
+                }
+            }
+
+
+            if (isContainsAnyLetter)
+            {
+                return sArray2[sArray2.Count - 1].Length;
+            }
+            else
+            {
+                return 0;   
+            }
+            
+            
+        }
+    }
+}
