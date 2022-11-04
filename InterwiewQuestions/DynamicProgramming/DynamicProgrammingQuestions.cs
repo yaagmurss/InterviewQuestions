357ea49a9121ce3b31e71c9cd9d9aa7e161029a1
Yagmur Solmaz <52479605+yaagmurss@users.noreply.github.com>
2022-11-04 13:04:21 +0300

    Add files via upload

---
+++ InterwiewQuestions/DynamicProgramming/DynamicProgrammingQuestions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InterwiewQuestions.DynamicProgramming
+{
+    public class DynamicProgrammingQuestions
+    {
+        public int ClimbStairs(int n)
+        {
+            //FIBONACCI SOLUTION
+
+            // base cases
+            if (n <= 0) return 0;
+            if (n == 1) return 1;
+            if (n == 2) return 2;
+
+            int one = 2;
+            int two = 1;
+            int temp = 0;
+
+            for (int i = 2; i < n; i++)
+            {
+                temp = one + two;
+                two = one;
+                one = temp;
+            }
+            return temp;
+
+
+        }
+    }
+}
---
+++ InterwiewQuestions/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+using System.Data;
+using InterwiewQuestions.LinkedLists;
+
+namespace InterwiewQuestions
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
