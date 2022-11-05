f5d58169dc559492cecf45d70e8203bbc4284b1a
Yagmur Solmaz <52479605+yaagmurss@users.noreply.github.com>
2022-11-05 11:40:14 +0300

    Add files via upload

---
+++ InterwiewQuestions/LinkedListsQuestions/RemoveFromLinkedList.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InterwiewQuestions.LinkedLists
+{
+    public class RemoveFromLinkedList
+    { 
+
+        /* Linked list Node*/
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+            public ListNode(int val = 0, ListNode next = null)
+            {
+                this.val = val;
+                this.next = next;   
+            }
+        }
+
+        public ListNode removeDuplicates(ListNode head)
+        {
+            /*Another reference to head*/
+            ListNode current = head;
+
+            /* Pointer to store the next 
+            pointer of a node to be deleted*/
+            ListNode next_next;
+
+            /* do nothing if the list is empty */
+            if (head == null)
+                return head;
+
+            /* Traverse list till the last node */
+            while (current.next != null)
+            {
+
+                /*Compare current node with the next node */
+                if (current.val == current.next.val)
+                {
+                    next_next = current.next.next;
+                    current.next = null;
+                    current.next = next_next;
+                }
+                else // advance if no deletion
+                    current = current.next;
+            }
+
+            return head;
+        }
+
+        /* Utility functions */
+
+        /* Inserts a new Node at front of the list. */
+        public ListNode push(int new_data)
+        {
+            /* 1 & 2: Allocate the Node &
+                    Put in the data*/
+            ListNode new_node = new ListNode(new_data);
+
+            ListNode head = new ListNode();
+
+            /* 3. Make next of new Node as head */
+            new_node.next = head;
+
+            /* 4. Move the head to point to new Node */
+            head = new_node;
+
+            return head;
+        }
+
+        /* Function to print linked list */
+        public void printList(ListNode head)
+        {
+            ListNode temp = head;
+            while (temp != null)
+            {
+                Console.Write(temp.val + " ");
+                temp = temp.next;
+            }
+            Console.WriteLine();
+        }
+
+    }
+}
---
+++ InterwiewQuestions/SortingQuestions/SortingQuestions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Security.Cryptography;
+using System.Text;
+using System.Xml.Linq;
+
+namespace InterwiewQuestions.SortingQuestions
+{
+    class SortingArrayQuestions
+    {
+        /// <summary>
+        /// You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, 
+        /// representing the number of elements in nums1 and nums2 respectively.
+        /// The final sorted array should not be returned by the function, 
+        /// but instead be stored inside the array nums1. 
+        /// To accommodate this, nums1 has a length of m + n, 
+        /// where the first m elements denote the elements that should be merged, 
+        /// and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
+        /// </summary>
+        public void Merge(int[] nums1, int m, int[] nums2, int n)
+        {
+            int i=0;    
+            int j=0;
+            while (nums1.Length > i)
+            {
+                if (nums1[i]==0 & i<m)
+                {
+                    nums1[i] = nums2[j];
+                    j++;
+                }
+                i++;
+            }
+            Array.Sort(nums1);
+        }
+    }
+}
