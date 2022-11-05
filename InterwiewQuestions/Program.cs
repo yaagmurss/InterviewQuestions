f35606b79160b078069daf507d8da21567df7df1
Yagmur Solmaz <52479605+yaagmurss@users.noreply.github.com>
2022-11-05 11:35:59 +0300

    Add files via upload

--- InterwiewQuestions/Program.cs
+++ InterwiewQuestions/Program.cs
@@ -4,6 +4,7 @@ using System.Text;
 using System.Globalization;
 using System.Data;
 using InterwiewQuestions.LinkedLists;
+using InterwiewQuestions.SortingQuestions;
 
 namespace InterwiewQuestions
 {
@@ -11,6 +12,19 @@ namespace InterwiewQuestions
     {
         static void Main(string[] args)
         {
+            var sortingArrayQuestions = new SortingArrayQuestions();
+
+            var nums1 = new int[] { -1, 0, 0, 3, 3, 3, 0, 0, 0 };
+            var nums2 = new int[] {1, 2, 2};
+
+            
+            sortingArrayQuestions.Merge(nums1,6,nums2,3);
+
+            foreach (var element in nums1)
+            {
+                Console.Write(element);
+            }
+
 
         }
     }
