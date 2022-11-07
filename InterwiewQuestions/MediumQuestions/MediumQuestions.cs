147bcb4479dedc5c29d10af002d6a4df255ed554
Yagmur Solmaz <52479605+yaagmurss@users.noreply.github.com>
2022-11-07 09:23:56 +0300

    Add files via upload
    medium type question added

---
+++ InterwiewQuestions/MediumQuestions/MediumQuestions.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace InterwiewQuestions.MediumQuestions
+{
+    public class MediumQuestion
+    {
+        /// <summary>
+        /// You are given a positive integer num consisting only of digits 6 and 9.        
+        /// Return the maximum number you can get by changing at most one digit(6 becomes 9, and 9 becomes 6).
+        /// </summary>
+        /// <param name="num"></param>
+        /// <returns></returns>
+
+        public int maximum69Number(int num)
+        {
+
+            int numCopy = num;
+            int indexSix = -1;
+            int currDigit = 0;
+
+
+            while (numCopy > 0)
+            {
+
+                if (numCopy % 10 == 6)
+                    indexSix = currDigit;
+
+
+                numCopy /= 10;
+                currDigit++;
+            }
+
+
+            return indexSix == -1 ? num : num + 3 * (int)Math.Pow(10, indexSix);
+        }
+    }
+        
+}
--- InterwiewQuestions/Program.cs
+++ InterwiewQuestions/Program.cs
@@ -5,6 +5,7 @@ using System.Globalization;
 using System.Data;
 using InterwiewQuestions.LinkedLists;
 using InterwiewQuestions.SortingQuestions;
+using InterwiewQuestions.MediumQuestions;
 
 namespace InterwiewQuestions
 {
@@ -12,18 +13,27 @@ namespace InterwiewQuestions
     {
         static void Main(string[] args)
         {
-            var sortingArrayQuestions = new SortingArrayQuestions();
+            //// SortingArrayQuestions
+            //var sortingArrayQuestions = new SortingArrayQuestions();
 
-            var nums1 = new int[] { -1, 0, 0, 3, 3, 3, 0, 0, 0 };
-            var nums2 = new int[] {1, 2, 2};
-
+            //var nums1 = new int[] { -1, 0, 0, 3, 3, 3, 0, 0, 0 };
+            //var nums2 = new int[] {1, 2, 2};
             
-            sortingArrayQuestions.Merge(nums1,6,nums2,3);
+            //sortingArrayQuestions.Merge(nums1,6,nums2,3);
 
-            foreach (var element in nums1)
-            {
-                Console.Write(element);
-            }
+            //foreach (var element in nums1)
+            //{
+            //    Console.Write(element);
+            //}
+
+            // MediumQuestions
+
+            var mediumQuestions = new MediumQuestion();
+
+            Console.WriteLine(mediumQuestions.maximum69Number(9669)); 
+
+
+
 
 
         }
