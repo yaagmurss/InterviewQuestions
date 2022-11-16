2b80ac7ddf3c709d76c105eb3893ce1fd67483ef
Yagmur Solmaz <52479605+yaagmurss@users.noreply.github.com>
2022-11-16 12:31:38 +0300

    Add files via upload

--- InterwiewQuestions/GraphTheoryQuestions/DepthFirstSearch.cs
+++ InterwiewQuestions/GraphTheoryQuestions/DepthFirstSearch.cs
@@ -4,77 +4,84 @@ using System.Text;
 
 namespace InterwiewQuestions.GraphTheoryQuestions
 {
-    public class DepthFirstSearch
+    public class Graph
     {
-            public int removeStones(int[][] stones)
+        private int V; // No. of vertices
+
+        // Array of lists for
+        // Adjacency List Representation
+        private List<int>[] adj;
+
+        // Constructor
+        Graph(int v)
+        {
+            V = v;
+            adj = new List<int>[v];
+            for (int i = 0; i < v; ++i)
+                adj[i] = new List<int>();
+        }
+
+        // Function to Add an edge into the graph
+        void AddEdge(int v, int w)
+        {
+            adj[v].Add(w); // Add w to v's list.
+        }
+
+        // A function used by DFS
+        void DFSUtil(int v, bool[] visited)
+        {
+            // Mark the current node as visited
+            // and print it
+            visited[v] = true;
+            Console.Write(v + " ");
+
+            // Recur for all the vertices
+            // adjacent to this vertex
+            List<int> vList = adj[v];
+            foreach (var n in vList)
             {
-                int n = stones.Length;
-                if (n <= 1)
-                {
-                    return 0;
-                }
-
-                var graph = new List<List<int>>{};
-                for (int i = 0; i < n; i++)
-                {
-                graph[i] = new List<int> { };
-                }            
-
-
-                for (int i = 0; i < n; i++)
-                {
-                    int[] u = stones[i];
-                    for (int j = 0; j < n; j++)
-                    {
-                        if (i == j)
-                        {
-                            continue;
-                        }
-
-                        int[] v = stones[j];
-                        if (u[0] == v[0] || u[1] == v[1])
-                        {
-                            graph[i].Add(j);
-                        }
-                    }
-                }
-
-            return n;
-
-
-            //    boolean[] visited = new boolean[n];
-            //    int ans = 0;
-
-            //    for (int i = 0; i < n; i++)
-            //    {
-            //        if (visited[i])
-            //        {
-            //            continue;
-            //        }
-
-            //        dfs(graph, visited, i);
-            //        ans++;
-            //    }
-
-            //    return n - ans;
-            //}
-
-            //private static void dfs(List<Integer>[] graph, boolean[] visited, int start)
-            //{
-
-            //    visited[start] = true;
-
-            //    List<Integer> neighbors = graph[start];
-            //    for (int x : neighbors)
-            //    {
-            //        if (visited[x])
-            //        {
-            //            continue;
-            //        }
-
-            //        dfs(graph, visited, x);
-            //    }
+                if (!visited[n])
+                    DFSUtil(n, visited);
             }
         }
+
+        // The function to do DFS traversal.
+        // It uses recursive DFSUtil()
+        void DFS(int v)
+        {
+            // Mark all the vertices as not visited
+            // (set as false by default in c#)
+            bool[] visited = new bool[V];
+
+            // Call the recursive helper function
+            // to print DFS traversal
+            DFSUtil(v, visited);
+        }
+
+        // Driver's Code
+        public static void Main(String[] args)
+        {
+            Graph g = new Graph(4);
+
+            g.AddEdge(0, 1);
+            g.AddEdge(0, 2);
+            g.AddEdge(1, 2);
+            g.AddEdge(2, 0);
+            g.AddEdge(2, 3);
+            g.AddEdge(3, 3);
+
+            Console.WriteLine(
+                "Following is Depth First Traversal "
+                + "(starting from vertex 2)");
+
+            // Function call
+            g.DFS(2);
+            Console.ReadKey();
+        }
     }
+
+    // This code is contributed by techno2mahi
+
+}
+    
 
