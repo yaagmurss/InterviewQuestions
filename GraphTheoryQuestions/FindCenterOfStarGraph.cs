d00395b40464d30ca7cd3955b9258142db109536
Yagmur Solmaz <52479605+yaagmurss@users.noreply.github.com>
2022-12-20 09:38:49 +0300

    FindCenterOfStarGraph Added

---
+++ GraphTheoryQuestions/FindCenterOfStarGraph.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Linq;
+
+namespace InterwiewQuestions.GraphTheoryQuestions
+{
+    /// <summary>
+    /// There is an undirected star graph consisting of n nodes labeled from 1 to n. 
+    /// A star graph is a graph where there is one center node and exactly n - 1 edges that connect the center node with every other node
+    /// You are given a 2D integer array edges where each edges[i] = [ui, vi] indicates that there is an edge between the nodes ui and vi
+    /// Return the center of the given star graph
+    /// </summary>
+    public class FindCenterOfStarGraph
+    {
+        public int findCenter(int[][] edges)
+        {
+            if (edges[0][0] == edges[1][0] || edges[0][0] == edges[1][1])
+            {
+                return edges[0][0];
+            }
+            else if(edges[0][1] == edges[1][0] || edges[0][1] == edges[1][1])
+            {
+                return edges[0][1];
+            }
+            else
+            {
+                return -1;
+            }
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -16,31 +16,41 @@ namespace InterwiewQuestions
         static void Main(string[] args)
         {
 
-            //int[][] grid = new int[][]
-            //{ 
+            //    //int[][] grid = new int[][]
+            //    //{ 
 
-            //    new int[]{0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0},
-            //    new int[]{0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0 },
-            //    new int[]{0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0 },
-            //    new int[]{0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0 },
-            //    new int[]{0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0 },
-            //    new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0 },
-            //    new int[]{0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0 },
-            //    new int[]{0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0 }
-                
-            //};
+            //    //    new int[]{0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0},
+            //    //    new int[]{0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0 },
+            //    //    new int[]{0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0 },
+            //    //    new int[]{0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0 },
+            //    //    new int[]{0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0 },
+            //    //    new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0 },
+            //    //    new int[]{0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0 },
+            //    //    new int[]{0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0 }
+
+            //    //};
+
+            //    int[][] grid = new int[][]
+            //    {
+            //        new int[]{ 0,0,0,0 },
+            //        new int[]{ 1,0,1,0 },
+            //        new int[]{ 0,1,1,0 },
+            //        new int[]{ 0,0,0,0 }
+            //    };
+
+            //    var numberOfEnclaves = new NumberOfEnclaves();
+
+            //    numberOfEnclaves.NumberOfEnclavesDepthFirstSearchApproach(grid);
 
             int[][] grid = new int[][]
-            {
-                new int[]{ 0,0,0,0 },
-                new int[]{ 1,0,1,0 },
-                new int[]{ 0,1,1,0 },
-                new int[]{ 0,0,0,0 }
-            };
+                {
+                    new int[]{ 1,2 },
+                    new int[]{ 2,3 },
+                    new int[]{ 4,2 }                    
+                };
 
-            var numberOfEnclaves = new NumberOfEnclaves();
-
-            numberOfEnclaves.NumberOfEnclavesDepthFirstSearchApproach(grid);
+            var findCenterOfStarGraph = new FindCenterOfStarGraph();
+            findCenterOfStarGraph.findCenter(grid);
 
         }
     }
